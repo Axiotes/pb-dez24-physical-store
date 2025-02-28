95e5903ed7a7aac250f44ecdfde4f4fa1609026a
Arthur Axiotes <axiotesarthur@gmail.com>
2025-02-28 16:00:40 -0300

    fix: remover arquivos nao utilizados do dist

--- dist/helper/order-by-distance.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.orderByDistance = void 0;
-const orderByDistance = (array) => {
-    let order = [];
-    array.forEach();
-};
-exports.orderByDistance = orderByDistance;
